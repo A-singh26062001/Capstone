f18ef8854e3cde54b3e698b2c01b38bf215f8aec
ira <iravashisht2002@gmail.com>
2023-08-24 13:42:17 +0530

    Field Verfication

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,68 +1,82 @@
-import React from 'react'
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import axios from 'axios'
-
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function Login() {
-    const [email, setEmail] = useState()
-    const [password, setPassword] = useState()
-    const navigate = useNavigate()
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(''); // State for error message
+    const navigate = useNavigate();
 
     axios.defaults.withCredentials = true;
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        axios.post('http://localhost:3001/login', {email, password})
-        .then(res => {
-            console.log(res);
-            if(res.data === "Success") {
-                navigate('/home')
-            }
-        }).catch(err => console.log(err))
-    }
 
-    return(
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // Check if email or password is empty
+        if (!email || !password) {
+            setError('Please fill in both email and password.');
+            return;
+        }
+
+        axios.post('http://localhost:3001/login', { email, password })
+            .then((res) => {
+                console.log(res);
+                if (res.data === 'Success') {
+                    navigate('/home');
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+                setError('An error occurred while logging in.');
+            });
+    };
+
+    return (
         <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
-      <div className="bg-white p-3 rounded w-25">
-        <h2>Login</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-3">
-            <label htmlFor="email">
-              <strong>Email</strong>
-            </label>
-            <input
-              type="email"
-              placeholder="Enter Email"
-              autoComplete="off"
-              name="email"
-              className="form-control rounded-0"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="email">
-              <strong>Password</strong>
-            </label>
-            <input
-              type="password"
-              placeholder="Enter Password"
-              name="password"
-              className="form-control rounded-0"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <button type="submit" className="btn btn-success w-100 rounded-0">
-            Login
-          </button>
-          </form>
-          <p>Already Have an Account</p>
-          <Link to="/register" className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none">
-            Sign Up
-          </Link>
-        
-      </div>
-    </div>
-    )
+            <div className="bg-white p-3 rounded w-25">
+                <h2>Login</h2>
+                {error && <div className="alert alert-danger">{error}</div>} {/* Display error message */}
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                        <label htmlFor="email">
+                            <strong>Email</strong>
+                        </label>
+                        <input
+                            type="email"
+                            placeholder="Enter Email"
+                            autoComplete="off"
+                            name="email"
+                            className="form-control rounded-0"
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="email">
+                            <strong>Password</strong>
+                        </label>
+                        <input
+                            type="password"
+                            placeholder="Enter Password"
+                            name="password"
+                            className="form-control rounded-0"
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <button type="submit" className="btn btn-success w-100 rounded-0">
+                        Login
+                    </button>
+                </form>
+                <p>Don't have an account?</p>
+                <Link
+                    to="/register"
+                    className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none"
+                >
+                    Sign Up
+                </Link>
+            </div>
+        </div>
+    );
 }
 
 export default Login;
